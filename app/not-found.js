6c091c8cf1c136306473ba40f2527096075e28f8
Nabilabintaakter <nabilabintaakter@gmail.com>
2025-01-23 22:23:12 +0600

    added a not found page

---
+++ app/not-found.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+
+  return (
+     <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
+            <p className='text-xl font-bold'> Sorry! The requested resource was not found.</p>
+        </div>
+  );
+};
+
+export default NotFound; 
