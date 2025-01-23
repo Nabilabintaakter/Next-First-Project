92378f205024c5adb260a7edb411aec317fa38eb
Nabilabintaakter <nabilabintaakter@gmail.com>
2025-01-24 02:04:38 +0600

    protected profile route using kindeAuth

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,16 +1,17 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 const Profile = async() => {
-    const { getUser } = getKindeServerSession();
+    const { getUser, isAuthenticated } = getKindeServerSession();
     const user = await getUser();
     return (
-        <main className='container mx-auto mt-8 my-5'>
-            <h1 className='text-3xl text-center mb-2 font-semibold'>Hi, {user.given_name}!</h1>
-            <p className='w-[90%] mx-auto text-center font-medium text-gray-500 mb-8 lg:mb-10'>
-            Welcome to your profile!
-            </p>
+       user && isAuthenticated ? <main className='container mx-auto mt-8 my-5'>
+        <h1 className='text-3xl text-center mb-2 font-semibold'>Hi, {user.given_name}!</h1>
+        <p className='w-[90%] mx-auto text-center font-medium text-gray-500 mb-8 lg:mb-10'>
+        Welcome to your profile!
+        </p>
 
-        </main>
+    </main>: redirect('api/auth/login')
     );
 };
 
