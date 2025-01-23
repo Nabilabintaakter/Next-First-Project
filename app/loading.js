4d22bcf2ab10b0ccedb7dca2707bf9077600b09a
Nabilabintaakter <nabilabintaakter@gmail.com>
2025-01-24 03:08:01 +0600

    fixed some issues

--- app/loading.js
+++ app/loading.js
@@ -3,8 +3,8 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 const Loading = () => {
     return (
         <div className='min-h-[calc(100vh-64px)] flex justify-center items-center'>
-            <p className='flex items-center gap-3 text-2xl'>
-                Please wait... <AiOutlineLoading3Quarters className="animate-spin text-2xl" />
+            <p className='flex text-black items-center gap-3 text-2xl'>
+                Please wait... <AiOutlineLoading3Quarters className="animate-spin text-black text-2xl" />
             </p>
         </div>
     );
