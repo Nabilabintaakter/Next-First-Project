2554d3ac29daa3313a1d10e147fd69885dd7d904
Nabilabintaakter <nabilabintaakter@gmail.com>
2025-01-23 22:16:22 +0600

    added a loading and error handling page

---
+++ app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Loading = () => {
+
+    return (
+        <div className=' flex justify-center items-center'>
+            <p className='text-xl font-bold'>Loading..............</p>
+        </div>
+    );
+};
+
+export default Loading;
